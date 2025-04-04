c3747bae0d12a0518b2cf07c54a608b449777bb8
cosmina24borangic <162466618+cosmina24borangic@users.noreply.github.com>
2025-04-04 12:54:47 +0300

    Initial commit  Next.js + Tailwind setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind funcționează! 
+      </h1>
+    </main>
+  );
+}
